e779ffe3c45c4fd760d291717e2921b8d364eaad
Alejandro Garza <alexhor22@gmail.com>
2019-04-06 15:10:34 -0600

    changed mongo

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# heroku_55k7w1dv
 
-MONGO_URL = os.environ.get('MONGOHQ_URL') 
+MONGO_URL = os.environ.get('MONGODB_URI') 
 client = MongoClient(MONGO_URL)
-db = client.app129241652
-collection = db.shoutouts
+db = client.heroku_55k7w1dv
+collection = db.TEST
 
 
 @app.route("/", methods=['GET']) 
 def index(): 
-	shouts = collection.find() 
+
+	shouts = collection.find()
 	return render_template('index.html', shouts=shouts)
 
 @app.route("/post", methods=['POST']) 
@@ -29,6 +31,18 @@ def post():
 def deleteAll(): 
 	collection.remove()
 	return redirect('/')
+
+# @app.route("/changeStatus", methods=['POST']) 
+# def post(): 
+# 	shout = {"status":request.form['status'], "adress":"", "name":""} 
+# 	shout_id = db['Tec'].insert(shout) 
+# 	return redirect('/')
+
+# @app.route("/createNew", methods=['POST']) 
+# def createNew(): 
+# 	shout = {"status":0, "adress":"", "name":""}
+# 	shout_id = db[request.form['colonia']].insert(shout)
+# 	return redirect('/')
 	
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000)) 
