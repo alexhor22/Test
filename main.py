2ff31c12f4fc69ae0226aed1ebf2afcec33add24
Alejandro Garza <alexhor22@gmail.com>
2019-04-06 15:40:07 -0600

    change status

--- main.py
+++ main.py
@@ -21,28 +21,35 @@ def index():
 	shouts = collection.find()
 	return render_template('index.html', shouts=shouts)
 
-@app.route("/post", methods=['POST']) 
-def post(): 
-	shout = {"name":request.form['name'], "message":request.form['message']} 
-	shout_id = collection.insert(shout) 
-	return redirect('/')
+# @app.route("/post", methods=['POST']) 
+# def post(): 
+# 	shout = {"name":request.form['name'], "message":request.form['message']} 
+# 	shout_id = collection.insert(shout) 
+# 	return redirect('/')
 
 @app.route("/deleteAll", methods=['GET']) 
 def deleteAll(): 
 	collection.remove()
 	return redirect('/')
 
-# @app.route("/changeStatus", methods=['POST']) 
-# def post(): 
-# 	shout = {"status":request.form['status'], "adress":"", "name":""} 
-# 	shout_id = db['Tec'].insert(shout) 
-# 	return redirect('/')
+@app.route("/changeStatus", methods=['POST']) 
+def post(): 
+	# shout = {"status":request.form['status'], "adress":"", "name":""} 
+	find = { "neighborhood": request.form['colonia'] }
+	values = { "$set": {
+	      "neighborhood": request.form['colonia'],
+	      "status": request.form['status'],
+	      "adress":"", "name":""
+	      } }
 
-# @app.route("/createNew", methods=['POST']) 
-# def createNew(): 
-# 	shout = {"status":0, "adress":"", "name":""}
-# 	shout_id = db[request.form['colonia']].insert(shout)
-# 	return redirect('/')
+	shout_id = collection.update_one(find,values) 
+	return redirect('/')
+
+@app.route("/createNew", methods=['POST']) 
+def createNew(): 
+	shout = {"neighborhood":request.form['colonia'],"status":0, "adress":"", "name":""}
+	shout_id = collection.insert(shout)
+	return redirect('/')
 	
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000)) 
